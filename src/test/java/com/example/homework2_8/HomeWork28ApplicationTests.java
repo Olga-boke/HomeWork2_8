03a3d243fbc067b607f38f51f591b27e9eda76f5
Olga <dochnohi@yandex.ru>
2022-10-07 22:23:11 +0300

    Initial commit

---
+++ src/test/java/com/example/homework2_8/HomeWork28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
